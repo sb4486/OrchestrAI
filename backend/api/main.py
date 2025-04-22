551cd82b7a0b885e4330c1c475b4e2e00833bf20
Sanchi Bansal <sb4486@drexel.edu>
2025-04-22 17:26:46 +0800

    refactor(api-server): remove unused lifespan

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,17 +1,7 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
 from api.routers import llms, mcps
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
-
-app = FastAPI(
-    lifespan=lifespan, swagger_ui_parameters={"tryItOutEnabled": True}
-)
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 app.include_router(llms.router, prefix="/v1")
 app.include_router(mcps.router, prefix="/v1")
