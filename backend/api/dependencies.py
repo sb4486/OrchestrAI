1bba211004a829a8ff8f379d6b6c6d2d91a9317e
Sanchi Bansal <sb4486@drexel.edu>
2025-04-23 15:09:13 +0800

    refactor(dependencies): cleanup dependencies

--- backend/api/dependencies.py
+++ backend/api/dependencies.py
@@ -1,23 +1,20 @@
-from typing import Annotated, Iterable
+from typing import Annotated
 
 from config import settings
 from fastapi import Depends
 from langchain_openai import ChatOpenAI
 
 
-def llm_factory() -> ChatOpenAI:
-    llm = ChatOpenAI(
+def get_llm() -> ChatOpenAI:
+    return ChatOpenAI(
         streaming=True,
         model=settings.model,
         temperature=0,
         api_key=settings.openai_api_key,
         stream_usage=True,
     )
-    return llm
 
 
-def get_llm_session() -> Iterable[ChatOpenAI]:
-    yield llm_factory()
+LLMDep = Annotated[ChatOpenAI, Depends(get_llm)]
 
 
-LLMDep = Annotated[ChatOpenAI, Depends(get_llm_session)]
