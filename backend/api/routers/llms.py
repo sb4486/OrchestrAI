af1c5377c19a0b8abbddd1bd1e04a974ac5b97a5
Sanchi Bansal <sb4486@drexel.edu>
2025-04-23 15:11:34 +0800

    feat(add agent endpoint):

--- backend/api/routers/llms.py
+++ backend/api/routers/llms.py
@@ -1,13 +1,40 @@
 from typing import AsyncGenerator
 
+import psycopg.errors
+from config import settings
 from fastapi import APIRouter
 from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from logs import print, uvicorn
+from psycopg_pool import AsyncConnectionPool
 from sse_starlette.sse import EventSourceResponse
 from starlette.responses import Response
 
-from api.dependencies import LLMDep, get_config, get_graph
+from api.agent.orchestration import get_config, get_graph
+from api.dependencies import LLMDep
 
 router = APIRouter(tags=["chat"])
+
+
+@router.get("/chat/completions")
+async def completions(query: str, llm: LLMDep) -> Response:
+    """
+    Stream model completions as Server-Sent Events (SSE).
+
+    This endpoint sends the model's responses in real-time as they are generated,
+    allowing for a continuous stream of data to the client.
+    """
+    return EventSourceResponse(stream_completions(query, llm))
+
+
+@router.get("/chat/agent")
+async def agent(query: str, llm: LLMDep) -> Response:
+    """Stream LangGraph completions as Server-Sent Events (SSE).
+
+    This endpoint streams LangGraph-generated events in real-time, allowing the client
+    to receive responses as they are processed, useful for agent-based workflows.
+    """
+    return EventSourceResponse(stream_graph(query, llm))
 
 
 async def stream_completions(
@@ -17,25 +44,33 @@ async def stream_completions(
         yield dict(data=chunk)
 
 
-@router.get("/chat/completions")
-async def completions(query: str, llm: LLMDep) -> Response:
-    """Stream completions via Server Sent Events"""
-    return EventSourceResponse(stream_completions(query, llm))
+async def checkpointer_setup(pool):
+    checkpointer = AsyncPostgresSaver(pool)
+    try:
+        await checkpointer.setup()
+    except (
+        psycopg.errors.DuplicateColumn,
+        psycopg.errors.ActiveSqlTransaction,
+    ):
+        uvicorn.warning("Skipping checkpointer setup — already configured.")
+    return checkpointer
 
 
 async def stream_graph(
     query: str,
     llm: LLMDep,
 ) -> AsyncGenerator[dict[str, str], None]:
-    graph = get_graph(llm)
-    config = get_config()
-    events = dict(messages=[HumanMessage(content=query)])
+    # NOTE: LangGraph AsyncPostgresSaver does not support SQLAlchemy ORM Connections.
+    # A compatible psycopg connection is created via the connection pool to connect to the checkpointer.
+    async with AsyncConnectionPool(
+        conninfo=settings.checkpoint_conn_str,
+        kwargs=dict(prepare_threshold=None),
+    ) as pool:
+        checkpointer = await checkpointer_setup(pool)
 
-    async for event in graph.astream_events(events, config, version="v2"):
-        yield dict(data=event)
+        graph = get_graph(llm, checkpointer=checkpointer)
+        config = get_config()
+        events = dict(messages=[HumanMessage(content=query)])
 
-
-@router.get("/chat/agent")
-async def agent(query: str, llm: LLMDep) -> Response:
-    """Stream LangGraph completions via Server Sent Events"""
-    return EventSourceResponse(stream_graph(query, llm))
+        async for event in graph.astream_events(events, config, version="v2"):
+            yield dict(data=event)
